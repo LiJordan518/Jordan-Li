94d94d7d8263eaa41035c29a2e78dbf2ff9bbf9b
LiJordan518 <49598541+LiJordan518@users.noreply.github.com>
2019-04-13 17:31:40 -0400

    Add files via upload

---
+++ Blue.js
@@ -0,0 +1,23 @@
+function s8(){
+    document.getElementById("s8").innerHTML = "BeebBeebBeebBeeebBeebBeeb";
+        
+}
+function t9(){
+    document.getElementById("s8").innerHTML = "Lettuce Beeb Beeb";
+}
+
+function windowAlert(){
+    window.alert("Hello! Beep Beep Lettuce");
+}
+
+function changeContent(){
+    document.getElementById("hello").innerHTML = "Beep Beep Lettuce";
+}
+
+function consoleLog(){
+    console.log("debugging = testing our codes, finding your bugs");
+}
+
+function documentWrite(){
+    document.write("I rewrote my whole page");
+}
